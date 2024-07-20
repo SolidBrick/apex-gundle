7d6ae2e2cfc66c3dabfb36f143c2376b7f62d88b
Terr <terrenceyang06@gmail.com>
2024-07-19 21:37:20 -0400

    Changed up and down arrows for triangles, imported all svg icons for guns.

--- src/ResultsTable.js
+++ src/ResultsTable.js
@@ -1,9 +1,10 @@
 import "./ResultsTable.css";
 export default function ResultsTable({
-  submittedNames, GUNDATA,
+  submittedNames,
+  GUNDATA,
   secretGunData,
   secretGun,
-  setGameWon
+  setGameWon,
 }) {
   function formatList(list) {
     if (list.length === 0) {
@@ -31,51 +32,106 @@ export default function ResultsTable({
     .slice()
     .reverse()
     .map((name) => {
-      const ammoMatch = checkMatching(GUNDATA[name].ammoType, secretGunData.ammoType);
-      const attachmentMatch = checkMatching(GUNDATA[name].attachments, secretGunData.attachments);
-      const firingModeMatch = checkMatching(GUNDATA[name].firingMode, secretGunData.firingMode);
+      const ammoMatch = checkMatching(
+        GUNDATA[name].ammoType,
+        secretGunData.ammoType
+      );
+      const attachmentMatch = checkMatching(
+        GUNDATA[name].attachments,
+        secretGunData.attachments
+      );
+      const firingModeMatch = checkMatching(
+        GUNDATA[name].firingMode,
+        secretGunData.firingMode
+      );
       if (submittedNames.includes(secretGun)) {
         setGameWon(true);
       }
-      return (<tr key={name}>
-        <td>{name}</td>
-        <td style={{backgroundColor: ammoMatch === "all" ? "rgba(0, 255, 0, 0.59)" : ammoMatch === "none" ? "rgba(255, 0, 0, 0.59)" : "rgba(219, 132, 13, 0.59)"}}>{formatList(GUNDATA[name].ammoType)}</td>
-        <td
-          style={{
-            backgroundColor:
-              GUNDATA[name].weaponClass === secretGunData.weaponClass
-                ? "rgba(0, 255, 0, 0.59)"
-                : "rgba(255, 0, 0, 0.59)",
-          }}
-        >
-          {GUNDATA[name].weaponClass}
-        </td>
-        <td
-          style={{
-            backgroundColor:
-              GUNDATA[name].releaseDate === secretGunData.releaseDate
-                ? "rgba(0, 255, 0, 0.59)"
-                : "rgba(255, 0, 0, 0.59)",
-          }}
-        >Season {GUNDATA[name].releaseDate} {secretGunData.releaseDate > GUNDATA[name].releaseDate ? "↑" : secretGunData.releaseDate < GUNDATA[name].releaseDate ? "↓" : ""}</td>
-        <td
-          style={{
-            fontSize: GUNDATA[name].attachments.length > 2 ? "85%" : "100%",
-            backgroundColor: attachmentMatch === "all" ? "rgba(0, 255, 0, 0.59)" : attachmentMatch === "none" ? "rgba(255, 0, 0, 0.59)" : "rgba(219, 132, 13, 0.59)"
-          }}
-        >
-          {formatList(GUNDATA[name].attachments)}
-        </td>
-        <td
-          style={{
-            backgroundColor:
-              GUNDATA[name].damagePerSecond === secretGunData.damagePerSecond
-                ? "rgba(0, 255, 0, 0.59)"
-                : "rgba(255, 0, 0, 0.59)",
-          }}
-        >{GUNDATA[name].damagePerSecond} {secretGunData.damagePerSecond> GUNDATA[name].damagePerSecond? "↑" : secretGunData.damagePerSecond< GUNDATA[name].damagePerSecond ? "↓" : ""}</td>
-        <td style={{backgroundColor: firingModeMatch === "all" ? "rgba(0, 255, 0, 0.59)" : firingModeMatch === "none" ? "rgba(255, 0, 0, 0.59)" : "rgba(219, 132, 13, 0.59)"}}>{formatList(GUNDATA[name].firingMode)}</td>
-      </tr>);
+      return (
+        <tr key={name}>
+          <td>
+            {name}
+            <img src="https://static.wikia.nocookie.net/apexlegends_gamepedia_en/images/7/7a/Sentinel_Icon.svg/revision/latest?cb=20230201232542" />
+          </td>
+          <td
+            style={{
+              backgroundColor:
+                ammoMatch === "all"
+                  ? "rgba(0, 255, 0, 0.59)"
+                  : ammoMatch === "none"
+                  ? "rgba(255, 0, 0, 0.59)"
+                  : "rgba(219, 132, 13, 0.59)",
+            }}
+          >
+            {formatList(GUNDATA[name].ammoType)}
+          </td>
+          <td
+            style={{
+              backgroundColor:
+                GUNDATA[name].weaponClass === secretGunData.weaponClass
+                  ? "rgba(0, 255, 0, 0.59)"
+                  : "rgba(255, 0, 0, 0.59)",
+            }}
+          >
+            {GUNDATA[name].weaponClass}
+          </td>
+          <td
+            style={{
+              backgroundColor:
+                GUNDATA[name].releaseDate === secretGunData.releaseDate
+                  ? "rgba(0, 255, 0, 0.59)"
+                  : "rgba(255, 0, 0, 0.59)",
+            }}
+          >
+            Season {GUNDATA[name].releaseDate}{" "}
+            {secretGunData.releaseDate > GUNDATA[name].releaseDate
+              ? "▲"
+              : secretGunData.releaseDate < GUNDATA[name].releaseDate
+              ? "▼"
+              : ""}
+          </td>
+          <td
+            style={{
+              fontSize: GUNDATA[name].attachments.length > 2 ? "85%" : "100%",
+              backgroundColor:
+                attachmentMatch === "all"
+                  ? "rgba(0, 255, 0, 0.59)"
+                  : attachmentMatch === "none"
+                  ? "rgba(255, 0, 0, 0.59)"
+                  : "rgba(219, 132, 13, 0.59)",
+            }}
+          >
+            {formatList(GUNDATA[name].attachments)}
+          </td>
+          <td
+            style={{
+              backgroundColor:
+                GUNDATA[name].damagePerSecond === secretGunData.damagePerSecond
+                  ? "rgba(0, 255, 0, 0.59)"
+                  : "rgba(255, 0, 0, 0.59)",
+            }}
+          >
+            {GUNDATA[name].damagePerSecond}{" "}
+            {secretGunData.damagePerSecond > GUNDATA[name].damagePerSecond
+              ? "▲"
+              : secretGunData.damagePerSecond < GUNDATA[name].damagePerSecond
+              ? "▼"
+              : ""}
+          </td>
+          <td
+            style={{
+              backgroundColor:
+                firingModeMatch === "all"
+                  ? "rgba(0, 255, 0, 0.59)"
+                  : firingModeMatch === "none"
+                  ? "rgba(255, 0, 0, 0.59)"
+                  : "rgba(219, 132, 13, 0.59)",
+            }}
+          >
+            {formatList(GUNDATA[name].firingMode)}
+          </td>
+        </tr>
+      );
     });
   return (
     <table className="ResultsTable">
@@ -83,7 +139,7 @@ export default function ResultsTable({
         <th style={{ width: "6em" }}>Gun</th>
         <th style={{ width: "5em" }}>Ammo Type</th>
         <th style={{ width: "5em" }}>Weapon Class</th>
-        <th style={{ width: "6em" }}>Release Date</th>
+        <th style={{ width: "7em" }}>Release Date</th>
         <th style={{ width: "10em" }}>Attachments</th>
         <th style={{ width: "6em" }}>Damage Per Second</th>
         <th style={{ width: "5em" }}>Firing Modes</th>
